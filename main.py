da24485160f065c3e3da0a2d8ab6ebf94286a2c8
sathvik divili <divilisathvik@gmail.com>
2024-12-16 18:21:49 -0800

    data transformation done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingpipeline
+from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingpipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 
@@ -8,6 +9,19 @@ try:
     logger.info(f"{STAGE_NAME} started")
     data_ingestion_pipeline = DataIngestionTrainingpipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Data Transformation Stage'
+
+try:
+
+    logger.info(f"{STAGE_NAME} started")
+    data_ingestion_pipeline = DataTransformationTrainingpipeline()
+    data_ingestion_pipeline.initiate_data_trasnformation()
     logger.info(f"{STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,7 @@
 from src.textSummarizer.constants import *
 from src.textSummarizer.utils.common import read_yaml, create_directories
+from src.textSummarizer.entity import DataTransformationConfig, DataIngestionConfig
 
-from src.textSummarizer.entity import DataIngestionConfig
 class ConfigurationManager:
     def __init__(self,
                  config_path=CONFIG_FILE_PATH,
@@ -24,3 +24,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_transformation_config(self)-> DataTransformationConfig:
+        config=self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name=config.tokenizer_name
+        )
+
+        return data_transformation_config
